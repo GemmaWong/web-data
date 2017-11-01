af39d10516556e985e6f933e4fa775c33ff42119
liangyefei <liangyefei@bonc.com.cn>
2017-11-01 16:03:02 +0800

    。。。。

--- src/web/components/Login.js
+++ src/web/components/Login.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 
 class Login extends React.Component {
-     render() {
-          return (
-               < div className = "login-box" >
-                    < div className = "login-title" > 登录 < / div >
-                    < div className = "login-line" >
-                         < div className = "left-box" > 用户名 < / div >
-                         < input className = "right-box" type="text" / >
-                    < / div >
-                    < div className = "login-line" >
-                         < div className = "left-box" > 密码 < / div >
-                         < input className = "right-box" type="password" / >
-                    < / div >
-                    < input className = "login-button"  type="button" value="登录"/ >
-               < / div >
-          );
-     }
+    render() {
+        return (
+            <div className="login-box">
+                <div className="login-title"> 登录</div>
+                <div className="login-line">
+                    < div className="left-box"> 用户名</div>
+                    < input className="right-box" type="text"/>
+                </div >
+                < div className="login-line">
+                    < div className="left-box"> 密码</ div >
+                    < input className="right-box" type="password"/>
+                </div >
+                <input className="login-button" type="button" value="登录"/>
+            </div >
+        );
+    }
 }
 export default Login;
